24cc830c4054dc2b3d4dfbf973f5829d4706d68d
Mjsims42 <150829417+Mjsims42@users.noreply.github.com>
2024-02-27 14:54:02 -0600

    Update init.cpp
    01

--- example/homework/01/init.cpp
+++ example/homework/01/init.cpp
@@ -1,14 +1,15 @@
+
 #include <Kokkos_Core.hpp>
 #include <cstdio>
-
+#include <iostream>
 // Problem: Link and run program with Kokkos where you initialize a View and print out its name with the $.label()$ method.
-
 int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   {
+  int n = 8;
   // Make View
-  
-  // print name
+  Kokkos::View<int*>A("Hello world",n);
+  std::cout << A.label() << "\n";
   }
   Kokkos::finalize();
 }
