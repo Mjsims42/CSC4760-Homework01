e2b773458b9885ea89629be2461dcf293fc2ee03
Mjsims42 <150829417+Mjsims42@users.noreply.github.com>
2024-02-28 06:27:24 -0600

    Update init.cpp

--- example/homework/03/init.cpp
+++ example/homework/03/init.cpp
@@ -1,15 +1,16 @@
+
 #include <Kokkos_Core.hpp>
 #include <cstdio>
-
+#include <iostream>
 // Declare a 5 ∗ 7 ∗ 12 ∗ n View
-
 int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   {
+  int n = 8;
   // Make View
-  
+  Kokkos::View<int****> A("A",5,7,12,n);
   // print name
-  
+  std::cout << A.label() << "\n";
   }
   Kokkos::finalize();
 }
