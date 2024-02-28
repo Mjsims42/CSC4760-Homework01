0b711705f73506f2ba1f989ffa189947f81eb5d2
Mjsims42 <150829417+Mjsims42@users.noreply.github.com>
2024-02-28 06:28:10 -0600

    Update init.cpp

--- example/homework/04/init.cpp
+++ example/homework/04/init.cpp
@@ -1,15 +1,29 @@
+
 #include <Kokkos_Core.hpp>
 #include <cstdio>
-
+#include <iostream>
 // Do simple parallel reduce to output the maximum element in a View
-
+struct Foo{
+  KOKKOS_INLINE_FUNCTION void operator() (const double& i, double& max) const{
+    if(i > max){
+            max = i;
+    }
+  }
+};
 int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   {
   // Make View and create values
-  
+        int n = 8;
+        Kokkos::View A("A",n);
+        Kokkos::parallel_for("Loop1", n, KOKKOS_LAMBDA (const int i) {
+                A(i) = i*i;
+        });
   // Do a parallel reduction
-  
+        Foo functor;
+        double result = 0;
+        Kokkos::parallel_reduce(A,functor,result);
+        std::cout << "Max output is" << result;
   }
   Kokkos::finalize();
 }
